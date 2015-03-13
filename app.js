be2faa6dab45b61893ef76d3132a5d26ce06eca1
Andrey Bakuta <anb@anahoret.com>
2015-03-13 13:51:55 +0200

    Added static assets server.

---
+++ app.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+var compression = require('compression')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(compression());
+
+app.use(function (req, res, next) {
+  if (path.extname(req.path).length > 0) {
+    next();
+  } else {
+    req.url = '/index.html';
+    next();
+  }
+});
+
+app.use(express.static(__dirname));
+
+module.exports = app;
