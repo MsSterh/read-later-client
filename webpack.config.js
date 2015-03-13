c189a58caf414232bda55081a2ab102315ab1135
Andrey Bakuta <anb@anahoret.com>
2015-03-13 20:58:39 +0200

    Add post install step.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-var webpack = require('webpack');
-
 module.exports = {
   entry: __dirname + "/src/js/main",
   devtool: 'source-map',
@@ -12,8 +10,5 @@ module.exports = {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
     ]
-  }//,
-  //plugins: [
-    //new webpack.optimize.UglifyJsPlugin({minimize: true})
-  //]
+  }
 }
